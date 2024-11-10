103764bd6dbc15eb8c6dbd42179148b5199fcb31
GevaYo <gevayo@mta.ac.il>
2024-11-10 21:36:57 +0200

    added a logger functionality

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 import userRoutes from "./routes/userRoutes";
 import authentication from "./middlewares/authentication";
 import testRoutes from "./routes/testRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
+import logger, { stream } from "./utils/logger";
 
 const app = express();
+
+app.use(morgan("combined", { stream }));
 
 app.use(express.json());
 app.use("/user", userRoutes);
@@ -12,6 +16,7 @@ app.use("/test", testRoutes);
 app.use("/pay", paymentRoutes);
 
 app.get("/", (req, res) => {
+  logger.info("GET / request received");
   res.send("Hello World");
 });
 
